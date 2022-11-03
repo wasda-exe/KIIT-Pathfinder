36d7b139b8aeb9ba07ecd087b19375f4b8f8a6ef
wasda-exe <wasda.exe@gmail.com>
2022-11-03 23:17:53 +0530

    Add location lists

--- assets/js/location.js
+++ assets/js/location.js
@@ -2,13 +2,46 @@ const campusMap = document.querySelector('.campusMap');
 const campusMapIframe = document.querySelector('.campusMapIframe');
 const campusMapA = document.querySelector('.campusMapA');
 
+const hostelMap = document.querySelector('.hostelMap');
+const hostelMapIframe = document.querySelector('.hostelMapIframe');
+const hostelMapA = document.querySelector('.hostelMapA');
+
+const activityMap = document.querySelector('.activityMap');
+const activityMapIframe = document.querySelector('.activityMapIframe');
+const activityMapA = document.querySelector('.activityMapA');
+
 const campusMapList = [
-    // {name: 'Campus6', lat: 20.35293, lon: 85.81803},
-    {name: 'Campus6', lat: 20.3529, lon: 85.8180},
-    // {name: 'Campus6', lat: 20.352, lon: 85.818},
-    // {name: 'Campus15', lat: 20.3493504, lon: 85.812258}
-    {name: 'Campus15', lat: 20.3493, lon: 85.8122}
+    {name:'Campus3', lat:20.36411796, lon:85.81004187, desc:`School of Civil Engineering, Electrical Engineering, chemistry department and some other departments inside this campus.
+    It is a beautiful place to hangout in evening. There is an Open air theater and a stage also for practicing drama, open mic poetry and other extracurricular activities. There is a cafeteria also for serving snacks and celebrating birthdays.`, addr:`10, KIIT Road, Chandaka Industrial Estate, K I I T University, Patia, Bhubaneswar, Odisha `},
+
+    {name:'Campus6', lat:20.35293, lon:85.81803, desc:`It is the main attraction of KIIT. It lies in all the photos of KIIT. Inside it lies the KIIT Convention Centre, the ICT Cell and the famous "I Love KIIT" spot. KIIT is incomplete without it.`, addr:`Campus 6, Chandaka Industrial Estate, K I I T University, Patia, Bhubaneswar, Odisha 751024, India`},
+
+    {name:'Campus7', lat:20.2674983, lon:85.8425316, desc:`Also known as KSOM, it is the Management Campus of KIIT. It is the most beautiful campus of KIIT. It hosts the famous Garden Library, where everything happens apart from studies. It also hosts the beautiful Sculpture Park and an Auditorium. Studying in this Campus is like a dream come true.`, addr:`KIIT School of Management, Campus-7, KIIT, Bhubaneswar 751024, Odisha, INDIA`},
+
+    {name:'Campus12', lat:20.35444275, lon:85.81897605, desc:`It is the Electronics Campus of KIIT. This is a beautiful campus, which hosts an Open Air Theatre and the beautiful Rose Garden. The Electronics Building also hosts the KIIT Robotics Society, one of the oldest and most important societies of KIIT.`, addr:`KIIT University, Prasanti Vihar Rd, Chandaka Industrial Estate, K I I T University, Patia, Bhubaneswar, Odisha 751024`},
+
+    {name:'Campus15', lat:20.3493504, lon:85.812258, desc:`It is the main campus of KIIT, the CSE Block. The driver branch of KIIT i.e. the CSE Branch students study here. There are two blocks, A block and B block. It also boasts of having an Open Air Theatre (OAT - 1), where students  showcase their hidden talents.`, addr:`Campus 15 Road, Chandaka Industrial Estate, K I I T University, Chandrasekharpur`}
+    // {name:'Campus3', lat:, lon:, desc:``, addr:``}
 ];
+
+const hostelMapList = [
+    {name:'KP1', lat:20.359916, lon:85.8212318, desc:``, addr:`9R3C+H3W, Shikhar Chandi Rd, Chandaka Industrial Estate, Patia, Bhubaneswar, Odisha 751024`},
+    {name:'KP4', lat:20.3463862, lon:85.7533722, desc:``, addr:`MCA-Hostel, KP-4, Sishu Vihar, Patia, Bhubaneswar, Odisha 751024`},
+    {name:'KP15', lat:20.3536721, lon:85.8158815, desc:``, addr:`Kalinga Institute of Industrial Technology, School of Electronics Engineering, KIIT School of Electronics, University, Chandaka Industrial Estate, Patia, Bhubaneswar, Odisha 751024`},
+    {name:'QC1', lat:20.3522058, lon:85.7473033, desc:``, addr:`9R28+VWP, Chandaka Industrial Estate, K I I T University, Patia, Bhubaneswar, Odisha 751024`},
+    {name:'QC4', lat:20.35286, lon:85.7482254, desc:``, addr:`KIIT, Shikhar Chandi Rd, Chandaka Industrial Estate, Bajrang Vihar, Shikhar Chandi Rd, Chandaka Industrial Estate, Bajrang Vihar, Patia, Bhubaneswar, Odisha 751024`},
+    // {name:'Campus3', lat:, lon:, desc:``, addr:``},
+];
+hostelMapList.forEach(element => {
+    element.desc = `Students can opt for one of three hostel accommodations: AC, Non-AC and AC with attached washrooms. The rooms are spacious with proper everyday cleaning. All hostel rooms have access to fast wifi (100+mbps). The meal quality is decent for the price charged. The menu consists of a lot of variety for both veg and non-veg consumers. Also, there are seperate hostels for boys and girls in every programme.`;
+});
+
+const activityMapList = [
+    {name:'KSAC', lat:20.3566019, lon:85.7488876, desc:``, addr:``},
+    // {name:'Campus3', lat:, lon:, desc:``, addr:``},
+];
+
+
 
 // console.log(campusMapIframe);
 // console.log(campusMapA);
